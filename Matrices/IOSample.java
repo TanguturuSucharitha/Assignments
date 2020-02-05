018ceb2d5462040e50c51bede7b3b1eb5fe954d3
TanguturuSucharitha <60088057+TanguturuSucharitha@users.noreply.github.com>
2020-02-05 00:26:20 -0800

    Add files via upload

---
+++ Matrices/IOSample.java
@@ -0,0 +1,37 @@
+
+public class IOSample {
+
+	public static void main(String[] args) {
+		String str="Suchi";
+		int n= str.length();
+		int [] freq =new int[n];
+		int i,j;
+		char string[]=str.toCharArray();
+		for(i=0;i<str.length();i++) {
+			freq[i]=1;
+		for(j=i+1;j<str.length();j++) {
+			if(string[i]==string[j]) {
+		
+			  freq[i]++;
+		      string[j]='0';
+			}
+			
+		}
+		}
+		System.out.println("chars n freqs");
+		for(i=0;i<freq.length;i++)
+			if(string[i]!=' ' && string[i]!='0')
+				System.out.println(string[i]+"-"+freq[i]);
+	}
+}
+	    
+	
+
+			
+		    
+		
+		
+
+	
+
+
